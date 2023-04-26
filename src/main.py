996f80782dbd99e5554c5e176dc8e3c2066429a5
Harish Vishwanath <hvishwanath@confluent.io>
2023-04-25 21:52:56 -0700

    Add TOC support for md output

--- src/main.py
+++ src/main.py
@@ -14,28 +14,44 @@ PRESERVE_WHITESPACE = True
 def process_org_model(model_file):
     pre_b = ""
     pre_e = ""
+    msg = list()
+    toc = list()
+    msg.append("toc")
     with open(model_file) as f:
         om = yaml.safe_load(f)
         og = OrgGraph(om)
         for k, v in og.models.items():
-            print(f"# Model Analysis: {k}")
+            header = f"Model Analysis: {k}"
+
+            toc.append(
+                f'- [{header}](#{header.lower().replace(" ", "-").replace("`","")})'
+            )
+
+            msg.append(f"# {header}")
             logging.debug(f"Graph ID: {v}")
             # print_msg_box(msg=f"Analyzing model {k}")
             oa = OrgGraphAnalyzer(v)
             oa.analyze()
             for a in oa.analysis:
-                print(f"## {a}")
+                toc.append(f'  - [{a}](#{a.lower().replace(" ", "-").replace("`","")})')
+                msg.append(f"## {a}")
                 for k, v in oa.analysis[a].items():
-                    print(f"### {k}")
+                    msg.append(f"### {k}")
+                    # toc.append(
+                    # f'    - [{k}](#{k.lower().replace(" ", "-").replace("`","")})'
+                    # )
                     if isinstance(v, str) or isinstance(v, int):
                         if "Reporting" in k:
-                            print(f"```\n{v}\n```")
+                            msg.append(f"```\n{v}\n```")
                         else:
-                            print(v)
+                            msg.append(str(v))
                     else:
-                        print(
+                        msg.append(
                             f'\n{pre_b}{tabulate(v, headers="firstrow", tablefmt="github")}{pre_e}\n'
                         )
+    # print("\n".join(msg))
+    print("\n".join(toc))
+    print("\n".join(msg))
 
 
 if "__main__" == __name__:
